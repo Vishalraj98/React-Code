32bb1752901abeeac4b124b2ad859217e2ca3e63
Vishalraj98 <vishalrajwadekar982@gmail.com>
2023-12-28 16:55:50 +0530

    Add 05File

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  //hooks ar e function in  it  we create in it 
+
+  return (
+    <>
+      kjhfjhsdabvg
+    </>
+  )
+}
+
+export default App
